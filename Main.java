fe9cb90fb152018003e3eab499d9090e2fb71124
Luca A <lucale1114@skola.goteborg.se>
2023-01-20 09:45:03 +0100

    Singleton assignment

--- Main.java
+++ Main.java
@@ -1,19 +1,7 @@
-import java.util.*;
-public class Main{
-  public static void main(String[] args){
-    List<Person> persons;
-    /*
-    persons.add(new Person("Bob", "bob@email.com"));
-    persons.add(new Person("Ben", "ben@email.com"));
-    persons.add(new Person("Pam", "pam@email.com"));
-    persons.add(new Person("Eve", "eve@email.com"));
-    persons.add(new Person("Guy", "guy@email.com"));
-    persons.add(new Person("Lis", "lis@email.com"));
-    persons.add(new Person("Ann", "ann@email.com"));
-    System.out.println(persons);
-    Storage.save(persons);
-    */
-    persons=Storage.fetchPersons();
-    System.out.println(persons);
-  }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(InstanceRootUser.getInstance());
+        System.out.println(LazyRootUser.getInstance());
+        System.out.println(EnumRootUser.INSTANCE);
+    }
 }
